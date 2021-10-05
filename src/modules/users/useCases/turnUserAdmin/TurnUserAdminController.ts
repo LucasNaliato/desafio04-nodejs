fc976ccc24f246f4dd449550fd975b3c4e5359b7
Lucas Naliato <lucasnaliato07@gmail.com>
2021-10-05 00:57:20 -0300

    Resolvendo desafio

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const newUser = this.turnUserAdminUseCase.execute({ user_id: String(user_id) });
+      return response.json(newUser);
+    } catch (error) {
+      return response.status(404).json({"error": error.message});
+    }
+    
   }
 }
 
